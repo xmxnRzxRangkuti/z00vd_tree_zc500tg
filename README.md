4289087c4377a2c3f7256a2fd54b497b23faabfe
xmxnRzxRangkuti <94756779+xmxnRzxRangkuti@users.noreply.github.com>
2023-03-21 18:28:47 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for asus ASUS_Z00VD (ASUS_Z00VD)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_ASUS_Z00VD-user
+add_lunch_combo omni_ASUS_Z00VD-userdebug
+add_lunch_combo omni_ASUS_Z00VD-eng
